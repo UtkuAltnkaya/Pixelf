c758a8c156d0b2b5f82375a0545e44be583ffaf8
UtkuAltnkaya <utkualt06@outlook.com>
2023-02-09 17:09:33 +0300

    Update CMake

---
+++ Image/src/Example.cpp
@@ -0,0 +1,11 @@
+#include "Example.hpp"
+#include <iostream>
+
+Example::Example()
+{
+  std::cout << "Example" << std::endl;
+}
+
+Example::~Example()
+{
+}
--- Image/src/main.cpp
+++ Image/src/main.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include "Example.hpp"
 
 using namespace cv;
+
 int main(int argc, char **argv)
 {
 
   Mat image;
-  image = imread("C:\\Users\\utkua\\Desktop\\Utku\\VsCode\\c++\\Example\\lenna.png");
+  image = imread("C:\\Users\\utkua\\Desktop\\Utku\\VsCode\\c++\\Pixelf\\Image\\lenna.png");
   if (!image.data)
   {
     printf("No image data \n");
@@ -15,5 +17,8 @@ int main(int argc, char **argv)
   namedWindow("Display Image", WINDOW_AUTOSIZE);
   imshow("Display Image", image);
   waitKey(0);
+
+  Example example{};
+
   return 0;
 }
