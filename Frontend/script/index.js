82d4235669609df0c133b3098e7014e74f498606
UtkuAltnkaya <utkualt06@outlook.com>
2023-02-18 18:51:07 +0300

    Modal continue

--- Frontend/script/index.js
+++ Frontend/script/index.js
@@ -4,24 +4,24 @@ const modalContainer = document.getElementById('modal-container');
 const resultImage = document.getElementById('image-result');
 const imageProcess = document.getElementById('image-process');
 
-modal.style.display = 'flex';
-document.body.style.overflow = 'hidden';
-modal.classList.add('modal');
-modalContainer.classList.add('modal-container');
+openModal.addEventListener('click', () => {
+  modal.style.display = 'flex';
+  document.body.style.overflow = 'hidden';
+  modal.classList.add('modal');
+  modalContainer.classList.add('modal-container');
 
-const image = new Image();
-image.src = 'image/bg.png';
+  const image = new Image();
+  image.src = 'image/bg.png';
 
-if (image.src) {
-  const imageWidth = image.width;
-  const imageHeight = image.height;
+  if (image.src) {
+    const imageWidth = image.width;
+    const imageHeight = image.height;
 
-  image.style.height = `${100 - 100 * (1 - imageHeight / imageWidth)}%`;
-  image.style.width = `${100 - 100 * (1 - imageWidth / imageHeight)}%`;
-  imageProcess.appendChild(image);
-}
-
-openModal.addEventListener('click', () => {});
+    image.style.height = `${100 - 100 * (1 - imageHeight / imageWidth)}%`;
+    image.style.width = `${100 - 100 * (1 - imageWidth / imageHeight)}%`;
+    imageProcess.appendChild(image);
+  }
+});
 
 // modal.addEventListener('click', () => {
 //   modal.classList.remove('modal');
